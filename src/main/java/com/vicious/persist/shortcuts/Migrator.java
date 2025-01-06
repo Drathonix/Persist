27035c5295b87eec9ceae583ff27f41605614de5
Drathonix <52723768+Drathonix@users.noreply.github.com>
2025-01-06 11:26:05 -0500

    1.2.4 - Flexibility and Future-proofing Improvements
    # 1.2.4 - Flexibility and Future-proofing Improvements
    * Added @AltName - Allows granting a savable element multiple names.
    * Added @ReplaceKeys - Allows replacing keys on map read using a path structure.
    
    (cherry picked from commit c6f85cb6e658f2d75d922669466d094e68b935ee)

--- src/main/java/com/vicious/persist/shortcuts/Migrator.java
+++ src/main/java/com/vicious/persist/shortcuts/Migrator.java
@@ -1,5 +1,7 @@
 package com.vicious.persist.shortcuts;
 
+import com.vicious.persist.Persist;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,13 +9,26 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 
+/**
+ * Shortcut class that handles automatic file format migration.
+ * @author Jack Andersen
+ * @since 1.2.0
+ */
 public class Migrator {
-    public static boolean migrate(NotationFormat format, String fileName) {
+    /**
+     * Migrates a file of any supported extension if it has the same extension-less name.
+     * The old file will be deleted and replaced with a new one in the correct format with the same data.
+     *
+     * @param format the format that will be migrated to.
+     * @param relativePath the path of the new file.
+     * @return whether migration was successful.
+     */
+    public static boolean migrate(NotationFormat format, String relativePath) {
         try {
             //TODO: this is a temp work-around, its not clean.
-            String pathFileName=Paths.get(fileName).getFileName().toString();
+            String pathFileName=Paths.get(relativePath).getFileName().toString();
             String filenameWithoutExtension = pathFileName.substring(0, pathFileName.lastIndexOf('.'));
-            File directory = new File(fileName).getParentFile();
+            File directory = new File(relativePath).getParentFile();
             File[] options = directory.listFiles();
             if(options != null) {
                 for (File option : options) {
@@ -22,7 +37,7 @@ public class Migrator {
                             if (value.isValidFile(option.getName())) {
                                 Map<String, Object> map = value.parser.mappify(Files.newInputStream(option.toPath()));
                                 option.delete();
-                                format.writer.write(map, Files.newOutputStream(new File(fileName).toPath()));
+                                format.writer.write(map, Files.newOutputStream(new File(relativePath).toPath()));
                                 return true;
                             }
                         }
@@ -30,6 +45,7 @@ public class Migrator {
                 }
             }
         } catch (IOException e) {
+            Persist.logger.warning("Could not migrate file");
             e.printStackTrace();
             return false;
         }
